7894d56e5df4724b183079783419084c8988d052
George Paul <atlasgp@umich.edu>
2022-03-07 23:09:50 -0500

    added functionality regarding plotting particles from particle filter while running robotSimulator in real mode. TODO: fix bug (line 211), update particle filter (line 194)

--- robotSimulator.py
+++ robotSimulator.py
@@ -62,11 +62,12 @@ class RobotSimulatorApp( JoyApp ):
     progress("Using %s:%d as the waypoint host" % self.srvAddr)
     self.T0 = self.now
     ### MODIFY FROM HERE ------------------------------------------
-    self.robSim = RobotSim(fn=None, app=self)
+    self.robSim = RobotSim(fn=None, app=self, sensor=self.sensor)
     self.move = MoveDistClass(self, self.robSim)
     self.liftWheels = LiftWheelsClass(self, self.robSim)
     self.turn = TurnClass(self, self.robSim)
     self.autoP = Auto(self, self.robSim, self.sensor)
+    self.robSim.plot()
 
   def showSensors( self ):
     """
